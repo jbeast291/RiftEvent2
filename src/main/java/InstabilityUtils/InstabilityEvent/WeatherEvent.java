599254e17d1087fbd2ac7ee97d2d1a943ee8501a
JoJeJo <109493932+JoJeJo@users.noreply.github.com>
2024-04-13 23:40:06 -0700

    Added Random Weather Event

---
+++ src/main/java/InstabilityUtils/InstabilityEvent/WeatherEvent.java
@@ -0,0 +1,19 @@
+package InstabilityUtils.InstabilityEvent;
+import GenericUtils.RandomUtils;
+import RiftEvent2.RiftEvent2;
+import org.bukkit.Bukkit;
+public class WeatherEvent {
+    public void Weather() {
+        //Selects and sends a random weather event
+        int RandInt = RandomUtils.Randomint(2,0);
+        if(RandInt == 0) {
+            Bukkit.getWorld(RiftEvent2.getInstance().WorldName).setStorm(false);
+        };
+        if(RandInt == 1) {
+            Bukkit.getWorld(RiftEvent2.getInstance().WorldName).setStorm(true);
+        };
+        if(RandInt == 2) {
+            Bukkit.getWorld(RiftEvent2.getInstance().WorldName).setThundering(true);
+        };
+    }
+}
